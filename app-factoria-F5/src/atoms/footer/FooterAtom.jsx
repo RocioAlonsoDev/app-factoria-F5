ef0673f904800e486becba0f133d11134b1aa136
SoleGrobas <solegrobas@yahoo.com.ar>
2023-08-16 16:09:56 +0200

    feature/FooterAtom ready to push

---
+++ app-factoria-F5/src/atoms/footer/FooterAtom.jsx
@@ -0,0 +1,38 @@
+import "./footerAtom.css";
+
+
+const FooterAtom = () => {
+    
+  return (
+    <>
+  
+        <div>
+            <div className="footer">
+            
+                <div className="footerText">
+                    <p className="paragrafFooter">Todos los derechos reservados 2021</p>
+                    <div className="footerLinks">
+                        <a href="https://factoriaf5.org/aviso-legal/">Aviso legal     </a>
+                        <p>|</p>
+                        <a href="https://factoriaf5.org/politica-de-privacidad/">Política de privacidad</a>
+                        <p>|</p>
+                        <a href="https://factoriaf5.org/politica-de-cookies/">Política de Cookies</a>
+                        
+                        <a href="https://factoriaf5.org/contacto/"> Contacta</a>
+                        <p>|</p>
+                        <a href="https://factoriaf5.org/somos/#transparencia">Transparencia</a>
+                        <p>|</p>
+                        <a href="https://factoriaf5.org/blog/">Blog</a>
+                        <p>|</p>
+                        <a href="https://www.crixa.es/">Diseño web Barcelona</a>
+                    </div>
+                </div>
+        
+            </div>
+        </div>
+ 
+   </>
+  )
+}  
+
+export default FooterAtom;
