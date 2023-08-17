957a83d5e2120c549fbdbe5c99e42b1c980db224
Marina <marieneflyaway@gmail.com>
2023-08-17 14:47:35 +0200

    navbar component

--- app-factoria-F5/src/App.jsx
+++ app-factoria-F5/src/App.jsx
@@ -1,11 +1,19 @@
 import { useState } from 'react'
 import './App.css'
 import ButtonAtom from './components/atoms/ButtonAtom/ButtonAtom'
+import NavbarAtom from './components/atoms/NavbarAtom/NavbarAtom'
+
  
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return (<ButtonAtom label={"inscripción"} color={"orangered"}></ButtonAtom>)
+  return (
+  <><NavbarAtom /><ButtonAtom label={"inscripción"} color={"orangered"}></ButtonAtom>
+ </>)
 }
-export default App
+
+
+
+
+export default App;
