e4a8c599377ff1d5bb5208c66e7db0ccd57b15de
AmaiiaAbaroa <abaroa.amaia@gmail.com>
2023-08-24 11:30:01 +0200

    UserProfilePage done user profile banner is missing

--- app-factoria-F5/src/App.jsx
+++ app-factoria-F5/src/App.jsx
@@ -3,10 +3,7 @@
 // import SignInSuccessPage from '../src/views/SignInSuccessPage/SignInSuccessPage';
 // import LandingPage from '../src/views/LandingPage/LandingPage';
 // import SearchFilterPage from "./views/SearchFilterPage/SearchFilterPage";
-import UserProfileTabsAtom from "./components/atoms/UserProfileTabsAtom/UserProfileTabsAtom";
-import UserProfileBodyFase1 from "./components/molecules/UserProfileBodyFases/UserProfileBodyFase1";
-import UserProfileBodyFase2 from "./components/molecules/UserProfileBodyFases/UserProfileBodyFase2";
-import UserProfileBodyFase3 from "./components/molecules/UserProfileBodyFases/UserProfileBodyFase3";
+import UserProfilePage from "./views/UserProfilePage/UserProfilePage";
 
 function App() {
   return (
@@ -15,11 +12,7 @@ function App() {
       {/* <SignInSuccessPage></SignInSuccessPage> */}
       {/* <LandingPage></LandingPage> */}
       {/* <SearchFilterPage></SearchFilterPage> */}
-      <UserProfileTabsAtom
-        element_fase1={<UserProfileBodyFase1></UserProfileBodyFase1>}
-        element_fase2={<UserProfileBodyFase2></UserProfileBodyFase2>}
-        element_fase3={<UserProfileBodyFase3></UserProfileBodyFase3>}
-      ></UserProfileTabsAtom>
+      <UserProfilePage></UserProfilePage>
     </>
   );
 }
