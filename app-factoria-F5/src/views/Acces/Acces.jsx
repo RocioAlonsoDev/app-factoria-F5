57c935cc1eb8073ff22f368ee645a3c2d88a1c92
Rocio <rocioalonso.dev@gmail.com>
2023-08-24 18:19:54 +0200

    started access page api consumption

--- app-factoria-F5/src/views/Acces/Acces.jsx
+++ app-factoria-F5/src/views/Acces/Acces.jsx
@@ -3,10 +3,33 @@ import  './Acces.css';
 
 
 function Acces() {
+
+    const FORM_ENDPOINT = "http://localhost/05-app-factoria-F5/back-php/API/applicants.php";
+    const navigate = useNavigate();
+        
+    const handleSubmit = (event) => {
+
+        event.preventDefault();
+
+        try {
+
+            axios.post(FORM_ENDPOINT,new FormData(event.target))
+            .then(response=>{
+                if(response.data){
+                    navigate('/success');
+                }
+            })
+        } catch (error) {
+            console.error("Error submitting form:", error);
+        }
+    };
+
+
+    
     return (
         <div id="form-container">
             <h2>Accede a tu cuenta</h2>
-            <form id='acces'>
+            <form id='acces' onSubmit={handleSubmit}>
                 <input type="email" name='Email' placeholder="Correo electrónico" />
                 <input type="password" name='Password' placeholder="Contraseña" />
                 <ButtonAtom label="Acceder" color="orange"/>
